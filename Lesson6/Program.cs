adc1feb3ddf77b12b1c42a8422201ae1d40431c8
Opilane TTHK <opilane@tthk.ee>
2019-10-10 16:54:23 +0300

    first commit

---
+++ Lesson6/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Write a beginning number");
+            int beginning_number = int.Parse(Console.ReadLine());
+            Console.WriteLine("Write an ending number");
+            int ending_number = int.Parse(Console.ReadLine());
+            Console.WriteLine("Write your number");
+            int your_number = int.Parse(Console.ReadLine());
+            for(int number=beginning_number; number<=ending_number; number++)
+            {
+                if (number == your_number)
+                {
+                    Console.WriteLine("In Range");
+                }
+                Console.WriteLine("Out of range");
+            }
+        }
+    }
+}
